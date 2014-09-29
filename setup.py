0f4cf80b3cdf8f212a932b5eda198861f7e33611
Kyle Gorman <kylebgorman@gmail.com>
2014-09-28 19:03:24 -0700

    sane setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="PPN",
+      version="1.0",
+      description="Perceptronix Point Never, a POS tagger",
+      author="Kyle Gorman",
+      author_email="gormanky@ohsu.edu",
+      url="http://github.com/cslu-nlp/PerceptronixPointNever/",
+      packages=["PPN"],)
