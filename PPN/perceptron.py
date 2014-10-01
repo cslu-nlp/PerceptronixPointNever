7c5e1bdb994f054422b4e0c3284eb89716d55201
Kyle Gorman <gormanky@ohsu.edu>
2014-10-01 16:35:03 -0700

    more fixes yet

--- PPN/perceptron.py
+++ PPN/perceptron.py
@@ -306,7 +306,7 @@ class SequencePerceptron(Perceptron):
                 for (y, yhat) in zip(yy, yyhat):
                     accuracy.update(y, yhat)
             logging.debug("Epoch {:>2} accuracy: ".format(epoch,
-                                   self._accuracy_str(epoch, accuracy)))
+                                 self._accuracy_str(accuracy)))
             logging.debug("Epoch {:>2} time elapsed: {}s.".format(epoch,
                                                    self._time_str(tic)))
 
